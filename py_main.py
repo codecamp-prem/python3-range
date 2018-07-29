355e7c48ff5456425abd2dffa6990f2177efc492
Prem Bikram Limbu <prem.bikram.limbu@gmail.com>
2018-07-29 17:15:36 +0530

    Create py_main.py

---
+++ py_main.py
@@ -0,0 +1,15 @@
+def starbox(width, height):
+    """print a box made up of asterisks.
+
+    width: width of box in characters, must be at least 2
+    height: height of box in lines, must be at least 2
+    """
+    print("*" * width) #print top edge of box
+
+    # print sides of box
+    for _ in range(height-2):
+        print("*" + " " * (width-2) + "*")
+
+    print("*" * width) #print bottom edge of box
+
+starbox(4, 3)
